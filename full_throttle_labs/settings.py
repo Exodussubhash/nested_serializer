06f8d8b50c91498ea5c3ff965f68c45b2e1ac9ba
subhash <poluru.subhash@infosys.com>
2020-06-04 10:57:37 -0500

    nested_serializer

--- full_throttle_labs/settings.py
+++ full_throttle_labs/settings.py
@@ -21,7 +21,6 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'jj@u+tv))@vyro_-kzftmd208ui6_gdtije1^v$_f01s_vmy2d'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
