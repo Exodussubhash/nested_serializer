f3c4a1c3f8cb754789bcee0111f825e3ccd60d72
subhash <poluru.subhash@infosys.com>
2020-06-04 07:31:03 -0500

    update

--- full_throttle_labs/settings.py
+++ full_throttle_labs/settings.py
@@ -42,18 +42,7 @@ INSTALLED_APPS = [
     'api_app',
 ]
 
-REST_FRAMEWORK = {
-    # Use hyperlinked styles by default.
-    # Only used if the `serializer_class` attribute is not set on a view.
-    'DEFAULT_MODEL_SERIALIZER_CLASS':
-        'rest_framework.serializers.HyperlinkedModelSerializer',
 
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissions',
-    ]
-}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
